8e57e2b69582775e8a5e15509768fb8d626dc93a
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-04-15 16:24:24 -0300

    refactor: remove ESLint configuration files and integrate new ESLint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,90 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
+import unusedImports from "eslint-plugin-unused-imports";
+import _import from "eslint-plugin-import";
+import filenames from "eslint-plugin-filenames";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([]), {
+    extends: fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-native/all",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+    )),
+
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+        "react-native": fixupPluginRules(reactNative),
+        "unused-imports": unusedImports,
+        import: fixupPluginRules(_import),
+        filenames,
+    },
+
+    languageOptions: {
+        globals: {
+            ...reactNative.environments["react-native"]["react-native"],
+        },
+
+        parser: tsParser,
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["error", {
+            selector: [
+                "variable",
+                "function",
+                "parameter",
+                "property",
+                "method",
+                "accessor",
+                "enumMember",
+                "objectLiteralProperty",
+            ],
+
+            format: ["camelCase"],
+            leadingUnderscore: "allow",
+            trailingUnderscore: "allow",
+
+            filter: {
+                regex: "^(__|[A-Z_]+)$",
+                match: false,
+            },
+        }, {
+                selector: "typeLike",
+                format: ["PascalCase"],
+            }],
+
+        "react/jsx-pascal-case": ["error"],
+        "no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": ["warn"],
+        "unused-imports/no-unused-imports": "warn",
+        "react/react-in-jsx-scope": "off",
+        "filenames/match-regex": ["error", "^[a-z][a-zA-Z0-9]+$", true],
+    }
+}]);
